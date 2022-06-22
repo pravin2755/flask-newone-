e99029950e4b9643524c4d5bee0b5ef3d7f6c335
pravinsinh.gohil <pravinsinh.gohil@msbcgroup.com>
2022-06-22 19:27:28 +0530

    completed frontend of registration page and login page using html,js and css.

--- my_app/form.py
+++ my_app/form.py
@@ -7,7 +7,10 @@ class UserRegistrationForm(UserCreationForm):
     first_name = forms.CharField(max_length=101)
     last_name = forms.CharField(max_length=101)
     email = forms.EmailField()
+    password1 = forms.CharField(max_length=150)
+    password2 =  forms.CharField(max_length=150)
+
 
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+        fields = [ 'first_name', 'last_name', 'email', 'password1', 'password2']
--- my_app/urls.py
+++ my_app/urls.py
@@ -1,6 +1,4 @@
-
 from django.contrib import admin
-
 
 from django.urls import path
 
@@ -8,9 +6,9 @@ from my_app.views import registerView, homeview, signin, SignUpView
 
 urlpatterns = [
 
-    path('register/',registerView, name='register'),
-    path('login/',signin, name='login'),
-    path('',homeview, name='home'),
-    path('cbase/',SignUpView.as_view(),name='signup')
+    path('register/', registerView, name='register'),
+    path('login/', signin, name='login'),
+    path('', homeview, name='home'),
+    path('cbase/', SignUpView.as_view(), name='signup')
 
 ]
