7d6c96acf29410d247810df89e9847c2caa773ce
Pravinsinh Gohil <pravinsinh.gohil@MSBC-MAINFRAME.LCL>
2022-07-28 19:57:56 +0530

    checkup

--- my_project/settings.py
+++ my_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-!c$dh56#72^ke&ihx57$s(d^%xp81y=zbr*^*e-u=iwd#)6-a^'
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "my_project.settings")
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = False
 DEBUG = True
