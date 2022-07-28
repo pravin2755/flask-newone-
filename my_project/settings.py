1ddd535560e57eba810b5ebded097d9c9fe45f05
Pravinsinh Gohil <pravinsinh.gohil@MSBC-MAINFRAME.LCL>
2022-07-28 18:18:53 +0530

    local path remove

--- my_project/settings.py
+++ my_project/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static", r'C:\Users\pravinsinh.gohil\Desktop\pravin\static']
+# STATICFILES_DIRS = [BASE_DIR / "static", r'C:\Users\pravinsinh.gohil\Desktop\pravin\static']
 STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
 
 
